a8228418807184a2e876687403242d8fbf11cdf0
Vashisth00 <vashisthbhushan@gmail.com>
2020-10-03 19:30:22 +0530

    Baseline only flutter installed

---
+++ beerbiceps/android/app/src/main/kotlin/com/example/beerbiceps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.beerbiceps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ beerbiceps/lib/screens/home/landing.dart
@@ -0,0 +1,74 @@
+// import 'package:flutter/material.dart';
+// import 'package:graphql_flutter/graphql_flutter.dart';
+
+// class Graphi extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final HttpLink httpLink =
+//         HttpLink(uri: "https://v3.beerbiceps.com/graphql");
+//     final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+//       GraphQLClient(
+//         // ignore: unnecessary_cast
+//         link: httpLink as Link,
+//         cache: OptimisticCache(
+//           dataIdFromObject: typenameDataIdFromObject,
+//         ),
+//       ),
+//     );
+//     return GraphQLProvider(
+//       child: HomePage(),
+//       client: client,
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   final String query = r"""
+//                     query PostsFetcg($code : String!){
+//                       continent(code:$code){
+//                         name
+//                         posts{
+//                            __typename
+// id
+// title
+// is_completed
+//                         }
+//                       }
+//                     }
+//                   """;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("GraphlQL Client"),
+//       ),
+//       body: Query(
+//         options: QueryOptions(
+//             // ignore: deprecated_member_use
+//             document: query,
+//             variables: <String, dynamic>{"code": "AS"}),
+//         builder: (
+//           QueryResult result, {
+//           VoidCallback refetch,
+//         }) {
+//           if (result.loading) {
+//             return Center(child: CircularProgressIndicator());
+//           }
+//           if (result.data == null) {
+//             return Text("No Data Found !");
+//           }
+//           return ListView.builder(
+//             itemBuilder: (BuildContext context, int index) {
+//               return ListTile(
+//                 title:
+//                     Text(result.data['continent']['countries'][index]['name']),
+//               );
+//             },
+//             itemCount: result.data['continent']['countries'].length,
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
