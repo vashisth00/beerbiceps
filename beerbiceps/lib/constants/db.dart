1a41558d4dc2068224246bd0d70824cf97e6b63e
Vashisth00 <vashisthbhushan@gmail.com>
2020-10-10 03:36:44 +0530

    Auth hide

---
+++ beerbiceps/lib/constants/db.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+void main() {
+  final HttpLink httpLink = HttpLink(
+    uri: 'https://beerbiceps2.adesignguy.co/graphql',
+  );
+
+  //Hide Auth from here
+  //final Link link = authLink.concat(httpLink);
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      cache: InMemoryCache(),
+      link: link,
+    ),
+  );
+}
