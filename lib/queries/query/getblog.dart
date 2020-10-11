ab050954d87101abf08bf97dd24eb837b9c1a356
Vashisth00 <vashisthbhushan@gmail.com>
2020-10-11 17:57:51 +0530

    Changes

--- lib/queries/query/getblog.dart
+++ lib/queries/query/getblog.dart
@@ -1,13 +1,15 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class FetchBlogs extends StatefulWidget {
-  @override
-  _FetchBlogsState createState() => _FetchBlogsState();
-}
+// class FetchBlogs extends StatefulWidget {
+//   @override
+//   _FetchBlogsState createState() => _FetchBlogsState();
+// }
 
-class _FetchBlogsState extends State<FetchBlogs> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+// class _FetchBlogsState extends State<FetchBlogs> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+
+// Will Come back later
